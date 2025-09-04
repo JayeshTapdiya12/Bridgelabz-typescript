89ec0142fc81727151bcc2aac22f104c6e56e355
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 19:26:44 +0530

    [jayesh tapdiya] added file for the arrays

---
+++ array.js
@@ -0,0 +1,10 @@
+// collection  of same data types
+// pre defined data types
+var user = ["jayesh", "tapdiya", "banglore"];
+var marks = [10, 20, 30, 40];
+var student = ["hello", "hiiii"];
+student.push("jayesh");
+console.log(student);
+// we can create the readonly array in which we cannot add the elements;
+var cllg = ["nit", "iit"];
+// cllg.push("medicaps")
---
+++ array.ts
@@ -0,0 +1,17 @@
+// collection  of same data types
+// pre defined data types
+
+var user: string[] = ["jayesh", "tapdiya", "banglore"];
+var marks: number[] = [10, 20, 30, 40];
+
+var student: string[] = ["hello", "hiiii"];
+
+student.push("jayesh");
+
+console.log(student);
+
+// we can create the readonly array in which we cannot add the elements;
+
+var cllg: readonly string[] = ["nit", "iit"];
+
+// cllg.push("medicaps")
