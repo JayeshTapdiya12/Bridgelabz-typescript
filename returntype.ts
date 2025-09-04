87694e4f38b4a78bc9b3ac3e988c47ea10d756fd
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 22:24:49 +0530

    [jayesh tapdiya]: added the return type in function

---
+++ returntype.ts
@@ -0,0 +1,22 @@
+// return type in function
+
+//  it is automatically detected by typescript
+// function fruit() {
+//   return "apple";
+// }
+
+// function fruit(): string {
+//   return "apple";
+// }
+
+// those function which does not return anything are called the void function
+
+function hello() {
+  console.log("hello");
+}
+
+function hello1(): void {
+  console.log("hello");
+}
+
+// if we dont know about the return type then we can use the ant data type
