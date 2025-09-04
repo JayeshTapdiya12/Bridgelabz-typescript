c083080b0a97dd68d5adb1615734e2b4a0477ec6
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 11:59:33 +0530

    [jayesh tapdiya]: added the string and boolean for the typescript

---
+++ stringbool.js
@@ -0,0 +1,15 @@
+var str = "how are you";
+var str2 = "how are you";
+var str3 = "how are you";
+var age = 30;
+var username = "jayesh tapdiya";
+var info = "my name is ".concat(username, " and age: ").concat(age, " years");
+console.log(info);
+// for converting the  into string .toString();
+var num = 100;
+var str4 = num.toString();
+// ************************boolean
+var item = true;
+var other;
+other = item;
+console.log(other);
---
+++ stringbool.ts
@@ -0,0 +1,21 @@
+var str: string = "how are you";
+var str2: string = "how are you";
+var str3: string = `how are you`;
+
+var age: number = 30;
+var username: string = "jayesh tapdiya";
+
+var info: string = `my name is ${username} and age: ${age} years`;
+console.log(info);
+
+// for converting the  into string .toString();
+
+var num: number = 100;
+var str4: string = num.toString();
+
+// ************************boolean
+
+var item: boolean = true;
+var other: boolean;
+other = item;
+console.log(other);
