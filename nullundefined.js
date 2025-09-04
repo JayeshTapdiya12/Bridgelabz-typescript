35156a60e16a5f86bc309ee6050f5a4b7a4e7c49
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 13:56:50 +0530

    [jayesh tapdiya] added : null and undefined

---
+++ nullundefined.js
@@ -0,0 +1,9 @@
+var user = null;
+var login = true;
+if (login) {
+    user = "hello";
+}
+console.log(user);
+// *******************************************************
+var item = undefined;
+//  for null == undefiend is true but for the ===  it is false
---
+++ nullundefined.ts
@@ -0,0 +1,12 @@
+var user: null | string = null;
+var login = true;
+if (login) {
+  user = "hello";
+}
+console.log(user);
+
+// *******************************************************
+
+var item: undefined = undefined;
+
+//  for null == undefiend is true but for the ===  it is false
