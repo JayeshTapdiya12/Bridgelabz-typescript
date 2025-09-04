74db04c87072f13882dc746a8f210310be19f54c
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 21:50:17 +0530

    [jayesh tapdiya] added file for the object and it property

---
+++ object.js
@@ -0,0 +1,32 @@
+// objext datatype:
+// var user: { name: string; age: number; company: string } = {
+//   name: "jayesh",
+//   age: 20,
+//   company: "BridgeLabz",
+// };
+// console.log(user);
+// user.name = "hello";
+// console.log(user);
+// making a dynanic object so that if we want to add any new property in future then we can do that:
+// var userData: {
+//   [key: string]: string | number | undefined | boolean;
+// } = {
+//   name: "jayesh",
+// };
+// userData.age = 20;
+// userData.company = "BridgeLabz";
+// userData.isMarried = false;
+// console.log(userData);
+// nexted object:
+// just definedd that quantity with the {} so that we can add any property in future:
+var user = {
+    name: "jayesh",
+    age: 20,
+    company: "BridgeLabz",
+    address: {
+        house: "abc",
+        number: "70",
+        city: "indore",
+    },
+};
+console.log(user);
