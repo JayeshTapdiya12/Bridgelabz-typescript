7e014e742a96d28b293033576e74132a6e61dfaa
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 22:47:05 +0530

    [jayesh tapdiya] added file for the parameter in the functions

---
+++ parameter.js
@@ -0,0 +1,17 @@
+//  parametere in function
+// function totalprice() {
+//   var price = 100;
+//   var item = 50;
+//   console.log(price * item);
+// }
+function totalprice(price, item, text) {
+    if (text) {
+        console.log(text + price * item);
+    }
+    else {
+        console.log(price * item);
+    }
+}
+totalprice(100, 50);
+totalprice(10, 50);
+totalprice(100, 70, "hello");
---
+++ parameter.ts
@@ -0,0 +1,20 @@
+//  parametere in function
+
+// function totalprice() {
+//   var price = 100;
+//   var item = 50;
+//   console.log(price * item);
+// }
+
+function totalprice(price: number, item: number, text?: string) {
+  if (text) {
+    console.log(text + price * item);
+  } else {
+    console.log(price * item);
+  }
+}
+totalprice(100, 50);
+totalprice(10, 50);
+totalprice(100, 70, "hello");
+
+// we can also use the any if we dont know the type of parameter
