ba3b028c1ecde6019e8b38430961f0e97d763757
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 20:03:34 +0530

    [jayesh tapdiya] added file for  the tupple

---
+++ tupple.ts
@@ -0,0 +1,16 @@
+//   A tuple is a typed array with a pre-defined length and types for each index.
+
+// it has a fixed length , ordered collection of elements , and each element in the tuple have a spedicified type
+
+var emp: string[] = ["jayesh", "tapdiya"];
+
+var empData: [string, number, boolean] = ["jayesh", 123, true];
+
+// if we push the element in the tuple it will add the elemenet but if we add in tuple array then it will  show the error
+
+empData.push("hello");
+console.log(empData);
+
+// it has same readonly tupple
+var empInfo: readonly [string, number, boolean] = ["jayesh", 123, true];
+empInfo.push("hello"); // it will show the error
