ac5bcdc7ec8e21959de3f1854f23e84b5983728e
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 14:59:16 +0530

    [jayesh Tapdiya ]: added bigint

---
+++ bigint.ts
@@ -0,0 +1,21 @@
+//  the bigint in the js and ts is  the maximum safe integer
+
+console.log("the bigint eg");
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+var number = 9007199254740991;
+var x = 1;
+var y = 2;
+
+console.log(number);
+console.log(number + x);
+console.log(number + y);
+
+//  to make the number into bigint we can simply write the bigint or we can place 'n' at the end of the number;
+
+// var number2=9007199254740991n
+var number2: bigint = 9007199254740991n;
+
+console.log(number2 + 1n);
+console.log(number2 + 2n);
