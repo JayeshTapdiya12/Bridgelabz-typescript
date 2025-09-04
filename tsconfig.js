c061a37ea753406371c62b052718fe16b40a7ffd
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-04 14:12:42 +0530

    [jayesh tapdiya]: some basic information on the tsconfig file

---
+++ tsconfig.js
@@ -0,0 +1,6 @@
+// in this file we will see the the tsconfig.json file details and use and how to modify them
+// to convert all the ts file into js you only need to code in terminal
+// tsc  (if you have the tsconfig.json file in your project)
+//  to make sure that all the converted file should be come in a single place we should assign a value to  outdir in the tsconfig
+// to save all thhe file there automaticlaly
+//  if we make declartion true then we will get all the declartion of the file in a new file with a extension of  .d.ts in the output folder
