be609b66bbaecb31fc83d2a0b3839515a6fd9d63
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-08 10:27:54 +0530

    [jayesh tapdiya] file added for the module

---
+++ module.js
@@ -0,0 +1,5 @@
+// module in typescript
+//   selfcontained piece of code
+//   which can be exported and imported in other module
+//   to use the functionality of that module
+// it is the normal import and export that we use
---
+++ module.ts
@@ -0,0 +1,6 @@
+// module in typescript
+//   selfcontained piece of code
+//   which can be exported and imported in other module
+//   to use the functionality of that module
+
+// it is the normal import and export that we use
